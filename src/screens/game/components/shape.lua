6d6dc9849c360b1ee1921062f9c728434b280a72
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-05-10 21:14:21 +0200

    compute dimensions

--- src/screens/game/components/shape.lua
+++ src/screens/game/components/shape.lua
@@ -15,6 +15,11 @@ Shape.__index = Shape
 function Shape:new(row, column, squares)
   local o = { row = row, column = column, squares = squares }
   setmetatable(o, self)
+
+  if #squares > 0 then
+    self.computeDimensions(o)
+  end
+
   return o
 end
 
@@ -24,15 +29,29 @@ function Shape:draw()
   end
 end
 
-function Shape:computeSize()
+function Shape:computeDimensions()
   local minRow = conf.PUZZLE_HEIGHT
   local maxRow = 0
   local minColumn = conf.PUZZLE_WIDTH
   local maxColumn = 0
 
   for _, square in ipairs(self.squares) do
-    square:draw()
+    if square.row > maxRow then
+      maxRow = square.row
+    end
+    if square.column > maxColumn then
+      maxColumn = square.column
+    end
+    if square.row < minRow then
+      minRow = square.row
+    end
+    if square.column < minColumn then
+      minColumn = square.column
+    end
   end
+
+  self.height = maxRow - minRow + 1
+  self.width = maxColumn - minRow + 1
 end
 
 return Shape
