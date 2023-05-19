0540ebc635f906045393403b1eaf97877c8e2135
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-05-20 00:18:36 +0200

    draw next player at tge right position

--- src/screens/game/sidebar/init.lua
+++ src/screens/game/sidebar/init.lua
@@ -1,10 +1,13 @@
 local Screen = require("engine.screen")
 local SidebarPainter = require("screens.game.sidebar.painter")
 local tetromino = require("screens.game.components.tetromino")
+local conf = require("conf")
+local colors = require("colors")
 
 ---@class Sidebar: Screen
 ---@field painter SidebarPainter
 ---@field nextPlayer Shape
+---@field nextPlayerCanvas love.Canvas
 local Sidebar = setmetatable({}, { __index = Screen })
 
 ---@param width number
@@ -16,13 +19,20 @@ function Sidebar:new(width, height)
 
   o.painter = SidebarPainter:new(width, height)
   o.nextPlayer = tetromino.randomTetromino()
-
+  o.nextPlayerCanvas = love.graphics.newCanvas(4 * conf.SQUARE_WIDTH, 2 * conf.SQUARE_WIDTH)
   return o
 end
 
 function Sidebar:paint()
   self.painter:drawBackground()
+
+  love.graphics.setCanvas(self.nextPlayerCanvas)
+  love.graphics.clear(colors.UiColors.SIDEBAR_BACKGROUND)
   self.nextPlayer:draw()
+
+  love.graphics.setCanvas(self.painter.canvas)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.draw(self.nextPlayerCanvas, conf.SQUARE_WIDTH, conf.SQUARE_WIDTH)
 end
 
 return Sidebar
