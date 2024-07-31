5a1173d8e1c5fc334a20105b3275851e341b4f4a
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2024-07-31 13:10:28 +0200

    just use canvas size on painter

--- src/screens/menu/painter.lua
+++ src/screens/menu/painter.lua
@@ -1,11 +1,12 @@
 local EnginePainter = require("engine.painter")
 local colors = require("colors")
+local conf = require("conf")
 
 ---@class MenuPainter: EnginePainter
 local Painter = setmetatable({}, { __index = EnginePainter })
 
 function Painter:new()
-  local o = EnginePainter:new()
+  local o = EnginePainter:new(conf.CANVAS_WIDTH, conf.CANVAS_HEIGHT)
   setmetatable(o, self)
   self.__index = self
   return o
